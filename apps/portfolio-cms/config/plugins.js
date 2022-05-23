06ba6cd9becf33ef7a16e472c59fb10b39d906dc
Jose Alves <jose.sousalves@gmail.com>
2022-05-23 15:07:41 +0100

    feat(cms): updates plugin file

--- apps/portfolio-cms/config/plugins.js
+++ apps/portfolio-cms/config/plugins.js
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 module.exports = ({ env }) => ({
   upload: {
     config: {
@@ -6,11 +7,27 @@ module.exports = ({ env }) => ({
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
+
+        localServer: {
+          maxage: 300000,
+        },
       },
       actionOptions: {
         upload: {},
         delete: {},
       },
     },
+  },
+  transformer: {
+    enabled: true,
+    config: {
+      prefix: '/api/',
+      responseTransforms: {
+        removeAttributesKey: true,
+      },
+    },
+  },
+  seo: {
+    enabled: true,
   },
 });
