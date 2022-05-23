1fbae0480f63b961ef1c02ef9e8568f0435e5881
Jose Alves <jose.sousalves@gmail.com>
2022-05-23 18:01:47 +0100

    feat(web): updates next pages setup

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,8 +1,49 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import { FC, useState, useEffect } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import { DefaultSeo } from 'next-seo';
+import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { Provider } from 'react-redux';
+import { GlobalStyle } from '../src/styles';
+import { ThemeProvider } from 'styled-components';
+import theme from 'src/styles/theme/theme';
+import SEO from '@config/next-seo';
+import { store } from 'src/redux/store';
+
+const MyApp: FC<AppProps> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  const [mounted, setMounted] = useState(false);
+  const { asPath } = useRouter();
+
+  const page = asPath === '/' ? '' : asPath;
+  const [canonicalUrl] = `https://radflix.com${page}`.split('?');
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <DefaultSeo
+            canonical={canonicalUrl}
+            openGraph={{
+              url: canonicalUrl,
+              type: 'website',
+              site_name: 'JA Portfolio',
+            }}
+            dangerouslySetAllPagesToNoIndex
+            {...SEO}
+          />
+          <GlobalStyle />
+          {mounted && <Component {...pageProps} />}
+        </ThemeProvider>
+      </Provider>
+    </>
+  );
+};
 
 export default MyApp;
---
+++ apps/web/src/config/next-seo.ts
@@ -0,0 +1,9 @@
+export default {
+  titleTemplate: '%s | JA Portfolio',
+  defaultTitle: 'José Eduardo Alves Portfolio',
+  twitter: {
+    handle: '@jose.sousalves',
+    site: '@jose.sousalves',
+    cardType: 'summary_large_image',
+  },
+};
