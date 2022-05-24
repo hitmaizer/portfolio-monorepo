b6a7a5d7336d8633825093072a1af243a4697817
Jose Alves <jose.sousalves@gmail.com>
2022-05-24 17:15:51 +0100

    feat(components): components folder index exports file

---
+++ apps/web/src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as Box } from './Box';
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as Grid } from './Grid';
+export { default as Homepage } from './Homepage';
+export { default as Intro } from './Intro';
+export { default as Stack } from './Stack';
+export { default as Text } from './Text';
