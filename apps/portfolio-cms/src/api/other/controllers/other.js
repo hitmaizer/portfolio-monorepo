19496be5868c288ea2341c875be9fed7f78c142e
Jose Alves <jose.sousalves@gmail.com>
2022-05-24 14:19:47 +0100

    refactor(cms): updates findOne method to search by slug

--- apps/portfolio-cms/src/api/other/controllers/other.js
+++ apps/portfolio-cms/src/api/other/controllers/other.js
@@ -1,9 +1,18 @@
-'use strict';
-
 /**
  *  other controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::other.other');
+module.exports = createCoreController('api::other.other', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+    if (!query.filters) query.filters = {};
+    query.filters.slug = { $eq: slug };
+    const entity = await strapi.service('api::other.other').find(query);
+    const { results } = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(results[0]);
+  },
+}));
--- apps/portfolio-cms/src/api/react/controllers/react.js
+++ apps/portfolio-cms/src/api/react/controllers/react.js
@@ -1,9 +1,18 @@
-'use strict';
-
 /**
  *  react controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::react.react');
+module.exports = createCoreController('api::react.react', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+    if (!query.filters) query.filters = {};
+    query.filters.slug = { $eq: slug };
+    const entity = await strapi.service('api::react.react').find(query);
+    const { results } = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(results[0]);
+  },
+}));
