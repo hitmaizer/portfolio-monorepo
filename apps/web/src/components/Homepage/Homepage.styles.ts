1b7d1876a32389d3f439dcb2f307b8aa45847d12
Jose Alves <jose.sousalves@gmail.com>
2022-05-24 15:34:52 +0100

    feat(Homepage): adds Homepage component

---
+++ apps/web/src/components/Homepage/Homepage.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Homepage = styled.div`
+  min-width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
