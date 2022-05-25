6bbc941d10edef108498f07ce11eb1fa38f86b13
Jose Alves <jose.sousalves@gmail.com>
2022-05-25 12:00:53 +0100

    feat(web): renders Hobbies component on Homepage

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,17 +1,26 @@
+import Hobbies from '@components/Hobbies';
 import { Homepage } from '@components/Homepage/Homepage.styles';
 import Intro from '@components/Intro';
 import Socials from '@components/Socials';
 import type { NextPage } from 'next';
 
-import { Grid } from '@components';
+import { Stack } from '@components';
 
 const Home: NextPage = () => {
   return (
     <Homepage>
-      <Grid>
+      <Stack
+        display="flex"
+        justifyContent="space-between"
+        gridGap="32px"
+        mt="32px"
+      >
         <Intro />
         <Socials />
-      </Grid>
+      </Stack>
+      <Stack mt="32px">
+        <Hobbies />
+      </Stack>
     </Homepage>
   );
 };
