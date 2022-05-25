ee14f529c5b0a68c3d7238b22fe541cd57472900
Jose Alves <jose.sousalves@gmail.com>
2022-05-25 10:17:26 +0100

    feat(Intro): adds Intro component markup

--- apps/web/src/components/Intro/Intro.tsx
+++ apps/web/src/components/Intro/Intro.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import Card from '@components/Card';
 import Heading from '@components/Heading';
 import Stack from '@components/Stack';
+import Text from '@components/Text';
+import Image from 'next/image';
 
 import * as S from './Intro.styles';
 import { IntroProps } from './Intro.types';
@@ -10,14 +13,48 @@ const Intro = ({ children, ...rest }: IntroProps) => {
   return (
     <>
       <S.Intro {...rest}>
-        <Stack display="flex" vertical>
-          <Stack display="flex">
-            <Stack display="flex" vertical>
-              <Heading>José Alves</Heading>
+        <Card>
+          <Stack display="flex" alignItems="center" gridGap="32px">
+            <Image src="/avatar-light.svg" width="200px" height="270px" />
+            <Stack display="flex" vertical gridGap="32px">
+              <Stack
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <Stack display="flex" vertical gridGap="8px">
+                  <Heading fontWeight="bold" size="2xl">
+                    José Alves
+                  </Heading>
+                  <Heading oxygen fontWeight="bold" size="lg">
+                    Front End Developer
+                  </Heading>
+                </Stack>
+                <Stack display="flex" vertical gridGap="8px">
+                  <Stack display="flex" alignItems="center" gridGap="8px">
+                    <S.EmailIcon />
+                    <Heading oxygen fontWeight="bold" size="lg">
+                      jose.sousalves@gmail.com
+                    </Heading>
+                  </Stack>
+                  <Stack display="flex" alignItems="center" gridGap="8px">
+                    <S.PhoneIcon />
+                    <Heading oxygen fontWeight="bold" size="lg">
+                      (+351) 91 102 340 5
+                    </Heading>
+                  </Stack>
+                </Stack>
+              </Stack>
+              <Text>
+                Self-taught developer, who is willing to learn and create
+                outstanding UI. <br />
+                On this page you will be able to get to know some of my work a
+                bit better.
+              </Text>
             </Stack>
+            {children}
           </Stack>
-        </Stack>
-        {children}
+        </Card>
       </S.Intro>
     </>
   );
