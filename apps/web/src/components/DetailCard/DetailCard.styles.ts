5feb9675eba94c0cafad3e64b5bf604e538f2c5e
Jose Alves <jose.sousalves@gmail.com>
2022-05-26 19:58:54 +0100

    style(DetailCard): adds 64px padding to DetailCard component

--- apps/web/src/components/DetailCard/DetailCard.styles.ts
+++ apps/web/src/components/DetailCard/DetailCard.styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const DetailCard = styled.div`
+  padding: 64px;
   background-color: ${({ theme }) => theme.body};
   min-height: 100vh;
   width: 100%;
