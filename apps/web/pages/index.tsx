5b4f3a42ba0c825b4df37d08a85dbd4da269f44a
Jose Alves <jose.sousalves@gmail.com>
2022-06-05 20:46:08 +0100

    feat(web): adds getStaticProps to fetch works data

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -4,11 +4,11 @@ import { Homepage } from '@components/Homepage/Homepage.styles';
 import Intro from '@components/Intro';
 import Socials from '@components/Socials';
 import Works from '@components/Works';
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage } from 'next';
 
 import { Stack } from '@components';
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ works }: any) => {
   return (
     <Homepage>
       <Stack display="flex" vertical gridGap="32px">
@@ -25,10 +25,22 @@ const Home: NextPage = () => {
           <Hobbies />
           <Certifications />
         </Stack>
-        <Works id="works" />
+        <Works id="works" worksData={works.data} />
       </Stack>
     </Homepage>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const request = await fetch(
+    'https://portfolio-monorepo.herokuapp.com/api/all-works?populate=*'
+  );
+  const works = await request.json();
+  return {
+    props: {
+      works,
+    },
+  };
 };
 
 export default Home;
