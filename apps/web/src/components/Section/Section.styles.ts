981f6cc36c2716bab248fa0ff43de6a18e3a78c0
Jose Alves <jose.sousalves@gmail.com>
2022-06-22 11:52:13 +0100

    feat(Section): add Section component styles file

---
+++ apps/web/src/components/Section/Section.styles.ts
@@ -0,0 +1,14 @@
+import { mediaQueries } from 'src/styles';
+import styled from 'styled-components';
+import { flexbox, gridGap, layout, space } from 'styled-system';
+
+export const Section = styled.section`
+  ${flexbox}
+  ${space}
+  ${layout}
+  ${gridGap}
+
+  ${mediaQueries.lg} {
+    flex-direction: row;
+  }
+`;
