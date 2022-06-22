00275ef6f2ae4e753a31e705cca4680e98c21fce
Jose Alves <jose.sousalves@gmail.com>
2022-06-22 16:16:44 +0100

    style(Certifications ): adds CertificationBox div

--- apps/web/src/components/Certifications/Certifications.styles.ts
+++ apps/web/src/components/Certifications/Certifications.styles.ts
@@ -1,5 +1,18 @@
+import { rem } from 'polished';
 import styled from 'styled-components';
+import { flexbox, gridGap, layout, space } from 'styled-system';
+
+import { CertificationsProps } from './Certifications.types';
 
 export const Certifications = styled.div`
   width: 100%;
+`;
+
+export const CertificateBox = styled.div<CertificationsProps>`
+  ${flexbox}
+  ${layout}
+  ${space}
+  ${gridGap}
+
+  min-width: ${rem('650px')};
 `;
